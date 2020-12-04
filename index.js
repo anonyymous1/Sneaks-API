635bae367daec1591d866f4d8846c6069036c0ec
Ruben Cedeno <71801837+anonyymous1@users.noreply.github.com>
2020-12-04 12:13:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('./routes/sneaks.routes.js')(app);
 require('dotenv').config();
 const SneaksAPI = require('./controllers/sneaks.controllers.js');
 
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
 mongoose.Promise = global.Promise;
 /*Sneaker.deleteMany({ }, function (err) {
   if(err) console.log(err);
@@ -15,9 +15,9 @@ mongoose.Promise = global.Promise;
 // Connecting to the database
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sneakers');
 
-app.listen(port, function () {
-  console.log(`Sneaks app listening on port `, port);
- });
+// app.listen(port, function () {
+//   console.log(`Sneaks app listening on port `, port);
+//  });
 
 module.exports = app;
 module.exports = SneaksAPI;
